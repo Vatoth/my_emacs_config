f4b53b0f40847ede0b088c1e44fb40c7cfea91a5
Quentin Sonnefraud <quentin.sonnefraud@epitech.eu>
2018-02-13 01:05:23 +0100

    a

---
+++ .emacs.d/elpa/elisp-format-20160508.252/elisp-format-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elisp-format" "20160508.252" "Format elisp code" 'nil :commit "03cc293eb2f78ec58fc1d84279af06816a04b979" :url "https://github.com/Yuki-Inoue/elisp-format")
---
+++ .emacs.d/elpa/xclip-1.4/xclip-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "xclip" "1.4" "use xclip to copy&paste" 'nil :url "http://elpa.gnu.org/packages/xclip.html" :keywords '("convenience" "tools"))
