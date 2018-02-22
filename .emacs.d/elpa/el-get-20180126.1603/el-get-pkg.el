21dbad2f387581994c59caf07d8d2db0142d3903
Quentin Sonnefraud <quentin.sonnefraud@epitech.eu>
2018-02-22 11:08:50 +0100

    a

---
+++ .emacs.d/elpa/el-get-20180126.1603/el-get-pkg.el
@@ -0,0 +1,5 @@
+(define-package "el-get" "20180126.1603" "Manage the external elisp bits and pieces you depend upon" 'nil :url "http://www.emacswiki.org/emacs/el-get" :keywords
+  '("emacs" "package" "elisp" "install" "elpa" "git" "git-svn" "bzr" "cvs" "svn" "darcs" "hg" "apt-get" "fink" "pacman" "http" "http-tar" "emacswiki"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
